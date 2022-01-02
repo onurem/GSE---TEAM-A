64f6b904692f12d6243fa6a48ec3e409d8b0e3c4
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2022-01-02 14:23:14 +0100

    Added type information where useful

--- sentimentDetector/Predictor.py
+++ sentimentDetector/Predictor.py
@@ -5,13 +5,13 @@ from sklearn.svm import LinearSVC
 class Predictor:
     clf: LinearSVC
 
-    def __init__(self, model_path, vectorizer_path):
+    def __init__(self, model_path: str, vectorizer_path: str):
         self.tfidf_vect = pickle.load(open(vectorizer_path, 'rb'))
         self.clf = pickle.load(open(model_path, 'rb'))
 
-    def predict(self, message: str):
+    def predict(self, message: str) -> (str, dict[str, float]):
         transformed = self.tfidf_vect.transform([message])
-        prediction = self.clf.predict(transformed)
+        prediction: str = self.clf.predict(transformed)
         raw_scores = self.clf._predict_proba_lr(transformed)
         scores = raw_scores[0]
         labeled_scores = dict(zip(self.clf.classes_, scores))
