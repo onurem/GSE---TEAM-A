46f5f9ae4ff1b5c6fd7a7b73d7cdb9a3cd6b359a
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2022-01-02 14:44:05 +0100

    Added doc strings for public functions

--- sentimentDetector/Predictor.py
+++ sentimentDetector/Predictor.py
@@ -12,6 +12,16 @@ class Predictor:
         self.clf = pickle.load(open(model_path, 'rb'))
 
     def predict(self, message: str) -> Prediction:
+        """Predicts the class of a string (e.g. offensive speech)
+        The classes are determined by model and vectorizer
+
+        :param message: str
+            The text that should be predicted
+        :returns:
+        Prediction
+            A prediction object holding the most likely class and
+            confidences for each class
+        """
         transformed = self.tfidf_vect.transform([message])
         predicted_class: str = self.clf.predict(transformed)
         raw_scores = self.clf._predict_proba_lr(transformed)
