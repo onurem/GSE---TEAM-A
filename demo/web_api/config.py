e5076439aabcfd98ae419bab019bc8c8b51ffb25
elchris <nguyenng178@gmail.com>
2022-01-12 09:14:42 +0700

    replace postgres uri to postgresql

--- demo/web_api/config.py
+++ demo/web_api/config.py
@@ -1,13 +1,18 @@
 import os
+
+from utils import convert_pg_uri
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'simple_secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',             \
-             'postgresql://localhost:5432/hateless_dev?user=dev_user&password=abc123456')
+    SQLALCHEMY_DATABASE_URI = convert_pg_uri(
+        os.environ.get('DATABASE_URL',
+                       'postgresql://localhost:5432/hateless_dev?user=dev_user&password=abc123456')
+    )
 
     def __repr__(self) -> str:
         return f"Config: {self.__dict__}"
---
+++ demo/web_api/utils.py
@@ -0,0 +1,2 @@
+def convert_pg_uri(old_uri: str) -> str:
+    return old_uri.replace('postgres', 'postgresql')
